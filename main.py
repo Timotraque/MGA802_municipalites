fc8dcc9d0be35aeddc1761fae0d6d63e50e32767
Timotraque <157532049+Timotraque@users.noreply.github.com>
2024-02-13 17:08:18 -0500

    update

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 #Fichier principal du programme
 
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 
 df = pd.read_csv("Census_2016_2021.csv")
@@ -19,13 +18,12 @@ print(moy_population)
 moy_population["Acroissement"] = (df_municipalites['Pop21'] - df_municipalites['Pop16']) / moy_population["Moyenne pop"]
 print(moy_population.head())
 
-#Affichage
+# Affichage
 plt.rcParams['font.size'] = 12
 plt.rcParams['figure.autolayout'] = True
 plt.rcParams['figure.dpi'] = 100
 
 plt.figure(figsize=(2.97 * 3, 2.1 * 3))
-#x_plot = np.linspace(0, max(moy_population.iloc[:,1]), len(moy_population)) #Créer autant de point que de lignes dans le df
 plt.scatter(moy_population.iloc[:, 1] * 100, moy_population.iloc[:, 2] * 100)
 plt.xlabel('Population moyenne')
 plt.ylabel('Acroissement en %')
