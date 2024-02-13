95c639f50e1cb9f1b96db6c08948c4144584312e
Timotraque <157532049+Timotraque@users.noreply.github.com>
2024-02-13 16:14:32 -0500

    sauvegarde
    Ca ne marche pas

--- main.py
+++ main.py
@@ -17,19 +17,30 @@ print(moy_population)
 
 # Calcul du pourcentage d'accroissement
 moy_population["Acroissement"] = (df_municipalites['Pop21'] - df_municipalites['Pop16']) / moy_population["Moyenne pop"]
-print(moy_population.head())
 
-#Affichage
+# Affichage
+"""
 plt.rcParams['font.size'] = 12
 plt.rcParams['figure.autolayout'] = True
 plt.rcParams['figure.dpi'] = 100
 
 plt.figure(figsize=(2.97 * 3, 2.1 * 3))
-#x_plot = np.linspace(0, max(moy_population.iloc[:,1]), len(moy_population)) #Créer autant de point que de lignes dans le df
 plt.scatter(moy_population.iloc[:, 1] * 100, moy_population.iloc[:, 2] * 100)
 plt.xlabel('Population moyenne')
 plt.ylabel('Acroissement en %')
 plt.title("Evolution de la population des municipalités du Québec")
 plt.grid()
-plt.show()
+plt.show()"""
 
+# Classement des municipalités
+
+"""df_classement = moy_population.loc[moy_population["Moyenne pop"] < 2000]
+df_classement.drop("Moyenne pop", axis='columns', inplace=True)
+df_classement.drop("Acroissement", axis='columns', inplace=True)
+df_classement.rename({"Nom": "<2000 hab"}, axis='columns', inplace=True)"""
+
+df_tampon = moy_population.loc[moy_population["Moyenne pop"] >= 2000 & moy_population["Moyenne pop"] < 10000]
+"""df_classement.drop("Moyenne pop", axis='columns', inplace=True)
+df_classement.drop("Acroissement", axis='columns', inplace=True)
+df_classement.rename({"Nom": "<2000 hab"}, axis='columns', inplace=True)"""
+print(df_tampon)
